649b08250ccf1902f62b58bf7dcf6e9251692e62
arbabcodess <9430187a@gmail.com>
2024-06-24 17:31:40 +0530

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.css';
 import Contact from './sections/Contact/Contact.jsx';
 import Hero from './sections/Hero.jsx';
 import Carousel from './sections/Projects.jsx';
@@ -7,35 +7,29 @@ import Footer from './sections/Footer/Footer.jsx';
 
 const slides = [
   {
-    image: '../public/fitlift.png',
+    image: '/images/fresh-burger.png',
     link: 'https://github.com/'
   },
   {
-    image: '../public/fresh-burger.png',
+    image: '/images/fitlift.png',
     link: 'https://github.com/'
-    
   },
   {
-    image: '../public/hipsster.png',
+    image: '/images/hipsster.png',
     link: 'https://github.com/'
   },
 ];
-function App() {
-  
 
+function App() {
   return (
-    <>
-   
     <div className="App">
-    <Hero/>
-      
+      <Hero />
       <Carousel slides={slides} />
-      < Skills/>
-      <Contact/>
-      <Footer/>
+      <Skills />
+      <Contact />
+      <Footer />
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
